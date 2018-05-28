726d4562cdc32b8e4e2ff89e4669054474a65349
EmilieBRG <emibgr@gmail.com>
2018-05-28 14:26:43 +0200

    service

--- src/AppBundle/Controller/FlightController.php
+++ src/AppBundle/Controller/FlightController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Flight;
+use AppBundle\Service\FlightInfo;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -64,13 +65,29 @@ class FlightController extends Controller
      * @Route("/{id}", name="flight_show")
      * @Method("GET")
      */
-    public function showAction(Flight $flight)
+    public function showAction(Flight $flight, FlightInfo $flightInfo)
     {
+        // calculate a flight distance between departure and arrival
+        $distance = $flightInfo->getDistance(
+            $flight->getDeparture()->getLatitude(),
+            $flight->getDeparture()->getLatitude(),
+            $flight->getArrival()->getLatitude(),
+            $flight->getArrival()->getLongitude()
+        );
+
+        //calculate a flight time with distance and speed
+        $time = $flightInfo->getTime(
+            $distance,
+            $flight->getPlane()->getCruiseSpeed()
+        );
+
         $deleteForm = $this->createDeleteForm($flight);
 
         return $this->render('flight/show.html.twig', array(
             'flight' => $flight,
             'delete_form' => $deleteForm->createView(),
+            'distance' => $distance,
+            'time' => $time
         ));
     }
 
